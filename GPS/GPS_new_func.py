7a45b65b6e0547618ef40d7fb3d0e482f4f762f7
Motoyamamoto <58988441+Motoyamamoto@users.noreply.github.com>
2019-12-25 14:31:25 +0200

    Update GPS_new_func.py

--- GPS/GPS_new_func.py
+++ GPS/GPS_new_func.py
@@ -1,12 +1,13 @@
 import os
+import GPS_point_Class
 from PIL import Image
 from PIL.ExifTags import TAGS, GPSTAGS
 
 os.chdir (r'C:\Users\AleksandrB\Documents\GitHub\Beetroot\GPS')
-
-try:  
-	original = Image.open('2019-08-21 17.33.37.jpg')  
-except FileNotFoundError:  
+mainpath = r'C:\Users\AleksandrB\Documents\GitHub\Beetroot\GPS'
+try:
+	original = Image.open('2019-08-21 17.33.37.jpg')
+except FileNotFoundError:
 	print('Файл не найден')
 
 
@@ -19,11 +20,57 @@ def getexif(filename):
 					for key in value.keys()}
 	return gpstags
 
-			
-print (getexif(original))
- 			
-#print (gpstags)
-#print (tag)
-#print (gpstag)
-#print("The size of the Image is: ")  
+def ftime(info):
+	if 'GPSTimeStamp' in info:
+		i = info ['GPSTimeStamp']
+		time = ( str(i[0][0]//i[0][1]) + ':'
+				+ str(i[1][0]//i[1][1]) + ':'
+				+ str(i[2][0]//i[2][1]))
+		return time
+
+def fdate(info):
+	if 'GPSDateStamp' in info:
+		j = info['GPSDateStamp'].split(':')
+		date = j[-1]+'.'+j[-2]+'.'+j[-3]
+		return date
+
+def get_coordinates(info):
+	for key in ['Latitude', 'Longitude']:
+		if 'GPS' + key in info and 'GPS' + key + 'Ref' in info:
+			e = info ['GPS' + key]
+			ref = info ['GPS' + key + 'Ref']
+			info [key] = ( str(int(e[0][0]/e[0][1])) + '°'
+						 + str(int(e[1][0]/e[1][1])) + '′'
+						 + str(e[2][0]/e[2][1]) + '″' + ref)
+		if 'Latitude' in info and 'Longitude' in info:
+			return [info['Latitude'], info['Longitude']]
+
+def get_decimal_coordinates(info):
+	for key in ['Latitude','Longitude']:
+		if 'GPS' + key in info and 'GPS' + key + 'Ref' in info:
+			e = info['GPS' + key]
+			ref = info['GPS' + key + 'Ref']
+			info[key] = (e[0][0]/e[0][1] +
+						 e[1][0]/e[1][1]/60 +
+						 e[2][0]/e[2][1]/3600) * (- 1 if ref in ['S','W'] else 1)
+	if  'Latitude' in info and 'Longitude' in info:
+		return [info['Latitude'], info['Longitude']]
+			 
+points = []
+for r, d, f in os.walk(mainpath):
+	for file in f:
+		if file.lower().endswith(('.png','.jpg','.jpeg')):
+			filepath = os.path.join(r, file)
+			picture = Image.open(filepath)
+			exif = getexif (picture)
+			latlong = get_decimal_coordinates (exif)
+			points.append (latlong)
+points.sort()
+#for i in range(len(points)):
+#	print (points[i])
+#print(fdate(getexif(original)))
+#print(ftime(getexif(original)))
+#print (get_coordinates (getexif(original)))
+#print (get_decimal_coordinates (getexif(original)))
+#print (getexif(original))
 #print(original.format, original.size, original.mode)
