7c4f38126d8207406c607cacdfee2f085eb06599
Motoyamamoto <58988441+Motoyamamoto@users.noreply.github.com>
2019-12-25 14:34:54 +0200

    Create HW_6_3_probe1.py

---
+++ Homeworks/Lesson 6/HW_6_3_probe1.py
@@ -0,0 +1,12 @@
+# List reverse
+
+list1 = ['Denza ','Faradenza', 'La', 'bokka', 'de', 'la', 'cokka',
+         'Grande', 'love', 'and', 'ribaua', 'villa', 'vida', 'loca']
+list2 = 'Grande'
+print (list1)
+
+for i in list2:
+    
+    list1 = [list2]
+
+print (list1)
