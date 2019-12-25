a227099b1391ad137628beba934d89e0dcd5d06f
Motoyamamoto <58988441+Motoyamamoto@users.noreply.github.com>
2019-12-25 14:35:13 +0200

    Create HW_5_1_act_1.py

---
+++ Homeworks/Lesson 5/HW_5_1_act_1.py
@@ -0,0 +1,32 @@
+# Exclusive common numbers
+import random
+
+#Generating of 2 lists of length 10 with random integers from 1 to 10
+
+list1 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+list1_rand = [random.randint(1,10) * 1 for list1_rand in list1]
+
+list2 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+list2_rand = [random.randint(1,10) * 1 for list2_rand in list2]
+
+print ('List 1: ', sorted(list1_rand))
+print ('List 2: ', sorted(list2_rand))
+
+#Cahanging types from list to set to compare the lists content
+
+list_all = list1_rand + list2_rand
+
+print (list_all)
+
+set_all = set (list_all)
+
+print (set_all)
+# Comparing the list1 and list2 content
+
+#set3_comp_result = set1_comp.intersection (set2_comp)
+
+# Creating a list with intersection numbers from list1 and list2
+
+#list3_comp_result = sorted (list (set3_comp_result))
+
+#print (list3_comp_result)
