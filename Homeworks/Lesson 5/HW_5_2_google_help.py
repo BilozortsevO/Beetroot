a9b97ddf4507bb0bc28a8aebcdc5a67b63f35dd3
Motoyamamoto <58988441+Motoyamamoto@users.noreply.github.com>
2019-12-25 14:35:10 +0200

    Create HW_5_2_google_help.py

---
+++ Homeworks/Lesson 5/HW_5_2_google_help.py
@@ -0,0 +1,11 @@
+# Extracting numbers
+# Creating a list with the range from 1 to 100
+
+list_ = list (range (1, 101))
+
+# Creating a list with integers from the list
+# that are divisible by 7 but not a multiple of 5
+
+result = [x for x in list_ if x % 7 == 0 and x % 5 != 0]
+
+print (result)
