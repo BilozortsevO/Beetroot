3b6416b50d7d8688103757354949ae4140e82826
Motoyamamoto <58988441+Motoyamamoto@users.noreply.github.com>
2019-12-25 14:35:21 +0200

    Create HW_lesson4_2_act.py

---
+++ Homeworks/Lesson 4/HW_lesson4_2_act.py
@@ -0,0 +1,5 @@
+#The birthday greeting program
+name = input ('Please enter the name: ', )
+BD = input ('Please enter your age: ', )
+BD = int (BD)
+print ("Hello", name, "on your next birthday you'll be", (BD+1), "years")
