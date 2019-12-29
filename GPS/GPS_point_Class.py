25a5e58e64e83b740a375ad975f5902e2956f1df
Motoyamamoto <58988441+Motoyamamoto@users.noreply.github.com>
2019-12-29 13:58:32 +0200

    Update GPS_point_Class.py

--- GPS/GPS_point_Class.py
+++ GPS/GPS_point_Class.py
@@ -1,3 +1,5 @@
+import math as m
+
 class Photo_data():
     def __init__ (self, file_name,date, time,
                   coordinates, deccoordinates,
@@ -8,9 +10,16 @@ class Photo_data():
         self.time = time
         self.coordinates = coordinates
         self.deccoordinates = deccoordinates
+
+    def aminusb (self, a, b):
+        self.distance = m.acos(
+                        m.sin(latA)*m.sin(latB) +
+                        m.cos(longA)*m.cos(longB)*m.cos(longA-longB)
+                        )
+
     def __str__ (self):
         return ('file_name: '+ self.file_name + '\n' + 
                 'date: ' + self.date + '\n' +
                 'time: ' + self.time + '\n' + 
-                'coordinates: ' + self.coordinates + '\n' +
-                'decimal coordinates: ' + self.deccoordinates)
+                'coordinates: ' + str(self.coordinates) + '\n' +
+                'decimal coordinates: ' + str(self.deccoordinates))
