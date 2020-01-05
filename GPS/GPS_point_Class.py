da7036fffbd5f617d22f9f57d27b939dfef32ee2
Motoyamamoto <58988441+Motoyamamoto@users.noreply.github.com>
2020-01-05 14:06:46 +0200

    Update GPS_point_Class.py

--- GPS/GPS_point_Class.py
+++ GPS/GPS_point_Class.py
@@ -3,23 +3,27 @@ import math as m
 class Photo_data():
     def __init__ (self, file_name,date, time,
                   coordinates, deccoordinates,
-                  distance1=None,distance2=None,
-                  distance3=None,distance4=None):
+                  distances=None):
         self.file_name = file_name
         self.date = date
         self.time = time
         self.coordinates = coordinates
         self.deccoordinates = deccoordinates
+        self.distances = distances
+        self.R = 6371 #Радиус Земли
 
-    def aminusb (self, a, b):
-        self.distance = m.acos(
-                        m.sin(latA)*m.sin(latB) +
-                        m.cos(longA)*m.cos(longB)*m.cos(longA-longB)
-                        )
+    def __sub__ (self, other):
+
+        self.distance = abs(self.R * m.acos(
+                        m.sin(m.radians(self.deccoordinates[1]))*m.sin(m.radians(other.deccoordinates[1])) +
+                        m.cos(m.radians(self.deccoordinates[1]))*m.cos(m.radians(other.deccoordinates[1]))*
+                        m.cos(m.radians(self.deccoordinates[0]-other.deccoordinates[0]))))
+        return self.distance
 
     def __str__ (self):
         return ('file_name: '+ self.file_name + '\n' + 
                 'date: ' + self.date + '\n' +
                 'time: ' + self.time + '\n' + 
                 'coordinates: ' + str(self.coordinates) + '\n' +
-                'decimal coordinates: ' + str(self.deccoordinates))
+                'decimal coordinates: ' + str(self.deccoordinates) + '\n' +
+                '4 nearest points: ' + str(self.distances))
